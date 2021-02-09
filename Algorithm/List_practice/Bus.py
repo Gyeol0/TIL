d2ee9e5bf58d2d91051b5f0cc9839fd4436ff6a1
gyeol <gksruf1128@naver.com>
2021-02-09 13:44:01 +0900

    List Practice

--- Algorithm/List_practice/Bus.py
+++ Algorithm/List_practice/Bus.py
@@ -1,8 +1,9 @@
 def Bus(n, m, k, arr):
     count = 0 # 충전 횟수
     current = 0 # 이전에 충전한 정류소
+    arr += [n]
     # 충전기가 있는 정류장을 돌면서 최대치 확인
-    for i in range(m-1):
+    for i in range(m):
         # 정류장 간의 거리가 k이상이면 0 반환(불가능)
         if arr[i+1] - arr[i] > k:
             return 0
@@ -10,10 +11,6 @@ def Bus(n, m, k, arr):
         if current + k < arr[i+1]:
             current = arr[i]
             count += 1
-    # 마지막 정류소 확인
-    if current + k < n:
-        current = arr[-1]
-        count += 1
     return count
 
 T = int(input())
